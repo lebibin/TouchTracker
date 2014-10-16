05ad473b0720be6d7af6136fd458beae604548a3
Kevin Candelaria <kevdashdev@gmail.com>
2014-10-16 14:31:47 +0800

    Initial Commit

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by Kevin Candelaria on 10/16/14.
+//  Copyright (c) 2014 Kevin Candelaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
